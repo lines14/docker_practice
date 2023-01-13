b1ad12266e00eb6b0316f962dc68e53098992448
Anton Lipatov <tonybass1414@yahoo.com>
2023-01-13 15:06:25 +0000

    refactor: remove trash

--- judicial_telegram_bot/modules/bot_base.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-from modules.config import TG_TOKEN
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
-storage = MemoryStorage()
-bot = Bot(token=TG_TOKEN)
-dp = Dispatcher(bot, storage=storage)
