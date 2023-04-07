12d55c39bbf5192834c687018b6ee79a7af88ead
Anton Lipatov <tonybass1414@yahoo.com>
2023-04-08 03:23:27 +0600

    flask http client

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='flask_templates')
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    return render_template('form.html')
 
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
